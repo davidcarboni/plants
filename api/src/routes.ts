aefb6dc96570d4a230ae8d67181908bf8d091ec2
David Carboni <david@carboni.io>
2024-06-02 15:56:19 +0100

    lint

--- api/src/routes.ts
+++ api/src/routes.ts
@@ -1,9 +1,15 @@
-/* eslint-disable no-use-before-define */
+
 import { Routes } from '@scloud/lambda-api/dist/types';
 import { ping } from './routes/ping';
+import list from './routes/list';
+import search from './routes/search';
+import get from './routes/get';
 
 const routes: Routes = {
   '/api/ping': { GET: ping },
+  '/api/list': { GET: list },
+  '/api/search': { GET: search },
+  '/api/plant': { GET: get },
 };
 
 export default routes;
